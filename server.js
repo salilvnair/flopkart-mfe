ff39d83adab091adce15fcc35b62ab8bf23ea34c
Salil V Nair <salilvnair@icloud.com>
2019-07-12 12:05:36 +0530

    server js dist name fix

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname+ '/dist/flopkart'))
+app.use(express.static(__dirname+ '/dist/flopkart-mfe'))
 let port = process.env.PORT || 8080;
 app.listen(port)
 
 app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + '/dist/flopkart/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/flopkart-mfe/index.html'));
 })
 
 console.log('Express listening to :'+port);
